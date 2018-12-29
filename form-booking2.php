7b9f6bfa2e64040ecee2cf666eb6b83f3d302521
diejesusdie <45885246+diejesusdie@users.noreply.github.com>
2018-12-30 02:01:47 +0300

    Add files via upload

---
+++ form-booking2.php
@@ -0,0 +1,15 @@
+﻿<?php if (isset($_POST["yourname"])) {
+$to =  "piligrimhotelnn@gmail.com";
+$from = 'otkogomail@mail.ru';
+$charset = "utf-8";
+$subject = '=?'.$charset.'?B?'.base64_encode("Сообщение с сайта").'?=';
+$headerss ="Content-type: text/html; charset=$charset\r\n";
+$name = '=?'.$charset.'?B?'.base64_encode("От пользователя").'?=';  //Имя_отправителя   c//
+$headerss.="From: $name <$from>\r\n";
+$headerss.="MIME-Version: 1.0\r\n";
+$headerss.="Date: ".date('D, d M Y h:i:s')."\r\n";
+$msg = "<h1>Бронирования комнаты отеля Пилигрим: ".$_POST["form_name"]." </h1><br>Имя: ".$_POST["yourname"].'<br>Телефон: '.$_POST["yourphone"].'<br>Дата заезда: '.$_POST["date_arrival"].'<br>Дата выезда: '.$_POST["date_departure"].'<br>Комната: '.$_POST["room_booking"];
+mail($to, $subject, $msg, $headerss);
+echo '<div class="good_send_echo"><p><span>Вы успешно забронировали комнату!</span> Менеджеры в скором времени свяжутся с вами!</p></div>';
+}
+?>
---
+++ form.php
@@ -0,0 +1,15 @@
+﻿<?php if (isset($_POST["yourname"])) {
+$to =  "sevenhotelnn@gmail.com";
+$from = 'otkogomail@mail.ru';
+$charset = "utf-8";
+$subject = '=?'.$charset.'?B?'.base64_encode("Сообщение с сайта").'?=';
+$headerss ="Content-type: text/html; charset=$charset\r\n";
+$name = '=?'.$charset.'?B?'.base64_encode("От пользователя").'?=';  //Имя_отправителя   c//
+$headerss.="From: $name <$from>\r\n";
+$headerss.="MIME-Version: 1.0\r\n";
+$headerss.="Date: ".date('D, d M Y h:i:s')."\r\n";
+$msg = "ФОРМА ОТПРАВКИ: ".$_POST["form_name"]." <br>ИМЯ: ".$_POST["yourname"].'<br>ТЕЛЕФОН: '.$_POST["yourphone"].'<br>ДАТА ЗАЕЗДА: '.$_POST["date_arrival"].'<br>ДАТА ВЫЕЗДА: '.$_POST["date_departure"].'<br>КОЛИЧЕСТВО ЧЕЛОВЕК: '.$_POST["people"];
+mail($to, $subject, $msg, $headerss);
+echo 'СООБЩЕНИЕ УСПЕШНО ОТПРАВЛЕНО!';
+}
+?>
